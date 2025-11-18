a0c26b7d1d6798f3cdb9ae6c3f8266233b193b67
DannielDelAngel <dannielehud@gmail.com>
2025-11-18 14:05:56 -0600

    not zomm

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,11 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+
+        <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+  />
         {/* Manifest */}
         <link rel="manifest" href="/manifest.json" />
 
--- app/page.tsx
+++ app/page.tsx
@@ -610,9 +610,9 @@ export default function HomePage() {
                           placeholder="Buscar"
                           value={searchTerm}
                           onChange={(e) => setSearchTerm(e.target.value)}
-                          className="w-full rounded-full border border-zinc-300 py-2 pl-10 pr-4 text-sm focus:outline-none focus:ring-2 focus:ring-orange-500"
+                          className="w-full rounded-full border border-zinc-300 text-zinc-700 py-2 pl-10 pr-4 text-sm focus:outline-none focus:ring-2 focus:ring-orange-500"
                         />
-                        <Search className="absolute left-3 top-2.5 text-zinc-500 w-5 h-5" />
+                        <Search className="absolute left-3 top-2.5 text-zinc-500 w-5 h-5 " />
                       </div>
 
                       {/* Lista de productos */}
@@ -929,7 +929,7 @@ export default function HomePage() {
                               +
                             </button>
  */}
-                            =
+                            
                             <button
                               onClick={() =>
                                 setCarrito((prev) =>
